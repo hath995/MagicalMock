3dd9659b77037b7ac07cd04a7862e37b4965b6cc
hath995 <hathlesstroubles@gmail.com>
2016-05-12 21:32:38 -0700

    added Mock prototype, added side_effect (raise, iteration, function)

--- src/mock.ts
+++ src/mock.ts
@@ -1,27 +1,85 @@
 
-
-class Mock {
+const UNSET = Symbol("undefined");
+const RETURN_VALUE = Symbol("return_value");
+const SIDE_EFFECT = Symbol("side_effect");
+let id = 1;
+export class Mock {
 
     constructor() {
         let mockfn = function() {};
+
+
         mockfn.called = false;
         mockfn.call_count = 0;
-        let unset = Symbol("undefined");
-        mockfn.__return_value = unset
+        mockfn.id = id++;
+        mockfn[RETURN_VALUE] = UNSET
         Object.defineProperty(mockfn, "return_value", {
-            get: function() {
-                if(this.__return_value == unset) {
+            get() {
+                if(this[RETURN_VALUE] == UNSET) {
                     return new Mock();
                 }else{
-                    return this.__return_value;
+                    return this[RETURN_VALUE];
                 }
             },
-            set: function(val) {
-                this.__return_value = val;
+            set(val) {
+                this[RETURN_VALUE] = val;
+            }
+        });
+        Object.defineProperty(mockfn, "side_effect", {
+            get() {
+                return this[SIDE_EFFECT];
+            },
+            set(val) {
+                if(val instanceof Function) {
+                    this[SIDE_EFFECT] = val;
+                else if(Array.isArray(val)) {
+                    this[SIDE_EFFECT] = (function*() {yield * val})();
+                }else{
+                    this[SIDE_EFFECT] = (function*() {yield val})();
+                }
             }
         });
         mockfn.call_args = undefined;
         mockfn.call_args_list = [];
         mockfn.method_calls = [];
+
+        let handler = {
+            apply: function(target, thisObj, args) {
+                target.called = true;
+                target.call_count++;
+                target.call_args = args;
+                target.call_args_list.push(args);
+                if(target[SIDE_EFFECT]) {
+                    if(target[SIDE_EFFECT] instanceof Function) {
+                        return target[SIDE_EFFECT](...args)
+                    }
+                    let {value, done} = target[SIDE_EFFECT].next();
+                    if(done) {
+                        throw Error("Side effect iteration ended");
+                    }
+                    if(value instanceof Error) {
+                        throw value;
+                    }
+                    return value;
+                }
+                return target.return_value;
+            },
+
+            get: function(target, property) {
+                //inspect seems to be a nodejs internal property; removing this test will enable a infinite loop
+                if(!(property in target) && property !== 'inspect') {
+                    target[property] = new Mock();
+                }
+                return target[property]
+            },
+            getPrototypeOf: function(target) {
+                return Mock.prototype
+            }
+
+        };
+        let proxy_obj = new Proxy(mockfn, handler);
+
+
+        return proxy_obj;
     }
 }
--- tests/test_mock.ts
+++ tests/test_mock.ts
@@ -45,22 +45,53 @@ describe("Mock", () => {
   });
 
   it("Should identify as a mock", () => {
-       let mock = new Mock();
-       expect(mock instanceof Mock).to.be.true;
+    let mock = new Mock();
+    expect(mock instanceof Mock).to.be.true;
   });
 
   it("Should return a mock for new properties", () => {
-       let mock = new Mock();
-       expect(mock.a).to.be.instanceof(Mock);
+    let mock = new Mock();
+    expect(mock.a).to.be.instanceof(Mock);
   });
 
   it("Should return a value for a set property", () => {
-      let mock = new Mock();
-      mock.a = true;
-      mock.b = 2;
-      mock.c.d = 3
-      expect(mock.a).to.be.true;
-      expect(mock.b).to.equal(2);
-      expect(mock.c.d).to.equal(3);
+    let mock = new Mock();
+    mock.a = true;
+    mock.b = 2;
+    mock.c.d.e = 3;
+    mock.f.g.h.return_value = false;
+    mock.h = [1,2,3];
+    expect(mock.a).to.be.true;
+    expect(mock.b).to.equal(2);
+    expect(mock.c.d.e).to.equal(3);
+    expect(mock.f.g.h()).to.be.false
+    expect(mock.h).to.deep.equal([1,2,3]);
   });
+
+  it("Should return a series of different values when given an array return value", () => {
+    let mock = new Mock();
+    mock.side_effect = [1,2,3];
+    expect(mock()).to.equal(1);
+    expect(mock()).to.equal(2);
+    expect(mock()).to.equal(3);
+  });
+
+  it("Should raise an exception when given a side_effect", () => {
+    let mock = new Mock();
+    mock.side_effect = RangeError("Mock has gone too far!");
+    expect(() => mock()).to.throw(RangeError);
+  });
+
+  it("Should throw an error at end of the side_effect", () => {
+    let mock = new Mock();
+    mock.side_effect = [1];
+    expect(mock()).to.equal(1);
+    expect(() => mock()).to.throw(Error);
+  })
+
+  it("Should call a function as a side_effect if given", () => {
+    let mock = new Mock();
+    mock.side_effect = (x,y) => x + y;
+    expect(mock(1,2)).to.equal(3);
+  })
 })
